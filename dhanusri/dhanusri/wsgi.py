a0fecf0b2b2ebe58e663df960f9bba5b8c95d70e
K DHANUSRI POOJA <dhanusripooja2007@gmail.com>
2025-04-11 13:29:34 +0530

    Success

---
+++ dhanusri/dhanusri/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dhanusri project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dhanusri.settings')
+
+application = get_wsgi_application()
